9100b3fe635d748f62de8be2425a8621df90fb75
AbdulazizErgashev <abdulazizergashev0335@gmail.com>
2025-06-11 10:11:33 +0500

    Navbar updated responsively

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,25 @@
 import { Link, NavLink } from "react-router-dom";
+import { useState } from "react";
 import Logo from "../assets/logo.png";
 import pages from "../pages/routes";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 
 export default function Navbar() {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+  const handleOpenMenu = () => setIsOpenMenu(!isOpenMenu);
+
   return (
-    <nav>
-      <div className="container mx-auto py-5 flex items-center justify-between">
-        <Link to={"/"} className="flex items-center gap-2 text-3xl font-bold">
+    <nav className="bg-[#819A91] fixed right-0 left-0 top-0 z-50">
+      <div className="container mx-auto py-5 px-5 md:px-0 flex items-center justify-between">
+        <Link
+          to={"/"}
+          className="flex items-center gap-2 text-xl md:text-3xl font-bold text-white"
+        >
           <img src={Logo} className="w-12" /> e-market
         </Link>
 
-        <ul className="flex items-center gap-10 text-lg font-semibold tracking-widest">
+        <ul className="hidden md:flex items-center gap-12 text-xl font-semibold tracking-widest text-white">
           {pages
             .filter((page) => page.showInNav && !page.icon)
             .map(({ name, path }) => (
@@ -20,18 +29,63 @@ export default function Navbar() {
             ))}
         </ul>
 
-        <div className="flex items-center gap-5">
+        <div className="hidden md:flex items-center gap-5">
           {pages
             .filter((page) => page.icon)
             .map(({ name, path, icon: Icon }) => (
-              <div key={name}>
-                <Link to={path} className="border-2 py-2 px-5 rounded-lg">
+              <div
+                key={name}
+                className="transition-all duration-300 active:scale-75"
+              >
+                <Link
+                  to={path}
+                  className="border-2 py-2 px-5 rounded-lg font-semibold bg-transparent text-white "
+                >
                   {name} <Icon />
                 </Link>
               </div>
             ))}
         </div>
+
+        <button onClick={handleOpenMenu} className="md:hidden text-white">
+          {isOpenMenu ? <CloseIcon /> : <MenuIcon />}
+        </button>
       </div>
+
+      {isOpenMenu && (
+        <div className="md:hidden p-5 flex items-center justify-between">
+          <ul className="md:hidden space-y-5 font-semibold tracking-widest text-white">
+            {pages
+              .filter((page) => page.showInNav && !page.icon)
+              .map(({ name, path }) => (
+                <li key={name}>
+                  <NavLink to={path} onClick={handleOpenMenu}>
+                    {name}
+                  </NavLink>
+                </li>
+              ))}
+          </ul>
+
+          <div className="md:hidden flex flex-col items-end gap-7">
+            {pages
+              .filter((page) => page.icon)
+              .map(({ name, path, icon: Icon }) => (
+                <div
+                  key={name}
+                  className="transition-all duration-300 active:scale-75"
+                >
+                  <Link
+                    to={path}
+                    className="border-2 py-2 px-5 rounded-lg font-semibold bg-transparent text-white"
+                    onClick={handleOpenMenu}
+                  >
+                    <Icon />
+                  </Link>
+                </div>
+              ))}
+          </div>
+        </div>
+      )}
     </nav>
   );
 }
