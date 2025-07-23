90785bcd3a3dfdcba5209e8fc1b839a56e6f8a75
AbdulazizErgashev <abdulazizergashev0335@gmail.com>
2025-07-23 10:36:58 +0500

    set footer component to bottom of the site

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import AppRoutes from "./pages/routes/Routes";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <>
       <Navbar />
       <AppRoutes />
+      <Footer />
     </>
   );
 }
